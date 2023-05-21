53319804a1400bd37eb3eb39ec4c4221be986608
Ralph Lawrence Bedayo <ralphbedayo009@gmail.com>
2023-05-21 14:20:00 +0800

    Fixed Vite side-effects

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from "path";
 
 export default defineConfig({
     plugins: [
@@ -19,4 +20,10 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+            '@img': path.resolve(__dirname, 'resources/img'),
+        },
+    }
 });
